44a6d68d6562db87fc8ea82fa20b66a6d8f28669
ArinBlak <arindamray.22@nshm.edu.in>
2025-12-15 23:50:55 +0530

    Add CORS middleware to FastAPI application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware 
 import tensorflow as tf
 import numpy as np
 from PIL import Image
